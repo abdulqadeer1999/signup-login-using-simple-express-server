1c66eff1a71111e067ab89f9c2416b267860ef2a
Abdul Qadeer Solangi <abdulqadeersolangi007@gmail.com>
2021-02-07 20:52:01 +0500

    working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-// const url = 'http://localhost:5000';
+const url = 'http://localhost:5000';
 
-const url = "https://new-login-signup.herokuapp.com"
+// const url = "https://new-login-signup.herokuapp.com";
 
 function postSignUp(){
     user = {
@@ -9,78 +9,27 @@ function postSignUp(){
         email : document.getElementById("email").value,
         password : document.getElementById("password").value,
 
-    }
-
-
-   const Http = new XMLHttpRequest();
-
-   Http.open("POST", url + "/signup");
-   Http.setRequestHeader("Content-Type" ,"application/json"),
-   Http.send(JSON.stringify(user));
-   Http.onreadystatechange =(e) => {
-       if(Http.readyState === 4){
-           let JsonResp = JSON.parse(Http.responseText);
-           if (JsonResp === 200){
-               document.getElementById("names").value= "";
-               document.getElementById("password").value= " ";
-               document.getElementById("email").value = " ";
-
-               alert(JsonResp.message);
-               window.location.href = "signin.html"
-           }else {
-               document.getElementById("names").innerHTML = JsonResp.message;
-               document.getElementById("email").innerHTML = JsonResp.message;
-               document.getElementById("password").innerHTML = JsonResp.message;
-           }
-       }
-   }
-  return false;
-}
-
-
-
-
-function login () {
-    let Emails = document.getElementById("email").value;
-    let Password = document.getElementById("password").value;
-
+    };
 
     const Http = new XMLHttpRequest();
-    Http.open("POST", url + "/signin")
-    Http.setRequestHeader("Content-Type","application/json")
 
-    Http.send(JSON.stringify( {
-        email: Emails,
-        password:Password,
-    }));
-
+    Http.open("POST", url + "/signup");
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify(user));
     Http.onreadystatechange = (e) => {
-        if(Http.readyState === 4){
-            let JSONres= JSON.parse(Http.responseText)
-            if (JSONres.status === 200) {
-                   document.getElementById("email").value = " ";
-                   document.getElementById("password").value = " ";
+        if (Http.readyState === 4) {
+            let JsonResp = JSON.parse(Http.responseText);
+            if (JsonResp.status === 200) {
+                document.getElementById('names').value = "";
+                document.getElementById('email').value = "";
+                document.getElementById('password').value = "";
 
-
-                   document.getElementById("Result").innerHTML = JSONres.message;
-                   document.getElementById("names").innerHTML = "Name :" + JSONres.user.name;
-                   document.getElementById("email").innerText = "Email :" +JSONres.user.email;
-            }
-
-            else if (JSONres.status>200) {
-                document.getElementById("Result").innerHTML = JSONres.message;
-                document.getElementById("names").innerHTML =  " " ;
-                document.getElementById("email").innerText =  " ";
-
-            }
-
-            else {
-                document.getElementById("Result").innerText = JSONres.message;
+                alert(JsonResp.message);
+                window.location.href = "signin.html";
+            } else {
+                document.getElementById("result").innerHTML = JsonResp.message;
             }
         }
     }
-    
     return false;
-
 }
-
